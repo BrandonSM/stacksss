9169bff53063a79d4dbddf6c0fbca283af50ef85
Brandon M <development@brandonmcginnis.com>
2017-08-14 21:12:47 -0400

    Updated ListReducer

---
+++ src/components/LibraryList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+
+		render () {
+			return(
+				return;
+			);
+		}
+}
+
+// calls the function connect(), turns into another function, then call that function with LibraryList.
+export default connect()(LibraryList);
--- src/reducers/LibraryReducer.js
+++ src/reducers/LibraryReducer.js
@@ -1,4 +1,6 @@
-// whenever the reducer runs, it will always return an array
-export default () => [
+// file extension with .json has to be specified. Webpack assumes all are .js unless otherwise specified.
+import data from './LibraryList.json'; 
 
-];
+// whenever the reducer runs, it will always return an array of data
+// data = array of objects .. eg... [{object},{object},{object}]
+export default () => data;
