6474cf1c0a1ee4b96480f1a0cfb73574e2407e4a
Brandon M <development@brandonmcginnis.com>
2017-08-15 20:25:03 -0400

    Added the LibraryList to the main app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux'; 
 import reducers from './reducers';
-import { Header } from './components';
+import { Header, LibraryList } from './components';
 
 const App = () => {
 	return (
 		<Provider store={createStore(reducers)}>
 		<View>
 			<Header headerText="Stackssss" />
+			<LibraryList/>
 		</View>
 		</Provider>
 	);
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -4,11 +4,15 @@ import { connect } from 'react-redux';
 class LibraryList extends Component {
 
 		render () {
-			return(
-				return;
-			);
+			return; 
 		}
 }
 
-// calls the function connect(), turns into another function, then call that function with LibraryList.
-export default connect()(LibraryList);
+// Purpose is to take global state object, and will map some part of the state to props.
+const mapStateToProps() = state => {
+ console.log(state);
+};
+
+// calls the function connect(), when connect is called it returns another function, 
+// then call that function with LibraryList. 2-step process
+export default connect(mapStateToProps)(LibraryList);
